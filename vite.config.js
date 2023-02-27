1001682124fb50babd7226a2f69d779903c90c28
TONY GNAVO <gnavo.tony@hotmail.fr>
2023-02-27 19:38:13 +0100

    Create a clean architecture folder structure

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [react()],
-    root: "src/App",
+    root: "src/UI/App",
     css: {
         modules: {
             localsConvention: "camelCase",
