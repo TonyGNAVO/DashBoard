bc222300bbc989903ba7e01897c8bf8065875974
TONY GNAVO <gnavo.tony@hotmail.fr>
2023-03-07 19:23:59 +0100

    Finish header

--- src/UI/Header/Header.tsx
+++ src/UI/Header/Header.tsx
@@ -1,4 +1,7 @@
 import HeaderCss from "./Header.module.scss"
+import userPhoto from "./../../Images/userPhoto.png"
+import searchIcon from "./../../Images/search.svg"
+import notificationIcon from "./../../Images/notification.svg"
 import { useParams } from "react-router-dom"
 
 const Header = () => {
@@ -6,12 +9,18 @@ const Header = () => {
     return (
         <header className={HeaderCss.header}>
             <h1 className={HeaderCss.headerTitle}>{tab ? tab : "overview"}</h1>
-            <div className={HeaderCss.headerSearchIcon}></div>
-            <div className={HeaderCss.headerNotificationIcon}></div>
+            <div className={HeaderCss.headerSearchIcon}>
+                <img className={HeaderCss.headerSearchIconImg} src={searchIcon} alt="search icon" />
+            </div>
+            <div className={HeaderCss.headerNotificationIcon}>
+                <img className={HeaderCss.headerNotificationIconImg} src={notificationIcon} alt="Notification icon" />
+            </div>
             <div className={HeaderCss.headerDivider}></div>
             <p className={HeaderCss.headerUserName}>Jones Ferdinand</p>
-            <div className={HeaderCss.headerUserPhoto}>
-                <div className={HeaderCss.headerUserPhotoWrapper}></div>
+            <div className={HeaderCss.headerUserPhotoMedia}>
+                <figure className={HeaderCss.headerUserPhotoMediaWrapper}>
+                    <img className={HeaderCss.headerUserPhotoMediaImg} src={userPhoto} alt="profile picture" />
+                </figure>
             </div>
         </header>
     )
