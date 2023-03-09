cd3b1056fd5bb92372b654ff2abe9345be7f8122
TONY GNAVO <gnavo.tony@hotmail.fr>
2023-03-09 20:26:59 +0100

    Move forward card

--- src/UI/MetricCard/MetricCard.tsx
+++ src/UI/MetricCard/MetricCard.tsx
@@ -1,10 +1,17 @@
+import { FC } from "react";
 import MetricCardCss from "./MetricCard.module.scss"
 
-const MetricCard = () => {
+type MetricCardProps = { title: string, value: number }
+const MetricCard: FC<MetricCardProps> = ({ title, value }) => {
 
     return (
         <div className={MetricCardCss.metricCard}>
-
+            <p className={MetricCardCss.metricCardTitle}>
+                {title}
+            </p>
+            <div className={MetricCardCss.metricCardValue}>
+                <p>{value}</p>
+            </div>
         </div>
     )
 }
--- src/UI/MetricCards/MetricCards.tsx
+++ src/UI/MetricCards/MetricCards.tsx
@@ -1,14 +1,23 @@
 import MetricCardCss from "./MetricCards.module.scss"
 import MetricCard from "../MetricCard/MetricCard"
 
+type cardProps = { id: number, title: string, value: number }
+
+const cards: cardProps[] = [
+    { id: 1, title: "Unresolved", value: 60 },
+    { id: 2, title: "Overdue", value: 16 },
+    { id: 3, title: "Open", value: 43 },
+    { id: 4, title: "On hold", value: 64 }
+]
+
 const MetricCards = () => {
 
     return (
         <div className={MetricCardCss.metricCards}>
-            <MetricCard />
-            <MetricCard />
-            <MetricCard />
-            <MetricCard />
+
+            {
+                cards.map(card => <MetricCard key={card.id} title={card.title} value={card.value} />)
+            }
         </div>
     )
 }
