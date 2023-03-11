d43740bc42c41e9b9af8e911d4b131714f9bb7da
TONY GNAVO <gnavo.tony@hotmail.fr>
2023-03-11 14:12:38 +0100

    Finish diagram  part

---
+++ src/UI/Diagram/Diagram.tsx
@@ -0,0 +1,31 @@
+import DiagramCss from "./Diagram.module.scss"
+import Graph from "./../../Images/Graph.png"
+const Diagram = () => {
+    console.log(DiagramCss)
+
+    return (
+        <>
+            <h2 className={DiagramCss.diagramTitle}>
+                Today's trends
+            </h2>
+            <p className={DiagramCss.diagramDescription}>
+                as of 25 May 2019, 09:41 PM
+            </p>
+            <div className={DiagramCss.diagramLabel}>
+                <div className={DiagramCss.diagramLabelItem}>
+                    <div className={`${DiagramCss.diagramLabelTodayStick} ${DiagramCss.stick} `}></div>
+                    <p>Today</p>
+                </div>
+                <div className={DiagramCss.diagramLabelItem}>
+                    <div className={`${DiagramCss.diagramLabelYesterdayStick} ${DiagramCss.stick} `}></div>
+                    <p>Yesterday</p>
+                </div>
+            </div>
+            <figure className={DiagramCss.diagramMedia}>
+                <img className={DiagramCss.diagramMediaImage} src={Graph} alt="Today's trands diagram"></img>
+            </figure>
+        </>
+    )
+}
+
+export default Diagram;
--- src/UI/MetricStats/MetricStats.tsx
+++ src/UI/MetricStats/MetricStats.tsx
@@ -1,10 +1,15 @@
 import MetricStatsCss from "./MetricStats.module.scss"
+import Diagram from "../Diagram/Diagram";
 const MetricsStats = () => {
 
 
     return (
         <div className={MetricStatsCss.metricStats}>
-
+            <div className={MetricStatsCss.metricStatsDiagram}>
+                <Diagram />
+            </div>
+            <div className={MetricStatsCss.metricStatsValue}>
+            </div>
         </div>
     )
 }
