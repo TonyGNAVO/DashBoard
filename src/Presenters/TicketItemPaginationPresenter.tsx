3fb4a40b53c3a8ebf17dacc26ee87c2899e01d04
TONY GNAVO <gnavo.tony@hotmail.fr>
2023-04-01 11:54:14 +0200

    Moving forwars Tickets Items

--- src/Presenters/TicketItemPaginationPresenter.tsx
+++ src/Presenters/TicketItemPaginationPresenter.tsx
@@ -6,30 +6,20 @@ import CSS from "csstype";
 export type TicketItemsType = {
     selectedItems: Ticket[];
     itemStyle: CSS.Properties;
-    profilStyle: CSS.Properties;
 };
 
 export class TicketItemPaginationPresenter
     implements PaginationPresenter<TicketItemsType>
 {
-    private readonly BOX_ITEMS_HEIGHT = 87;
-    private readonly PROFIL_PHOTO_DEFAULT_DIMENSION = 4.4;
-    private readonly NUMBER_PER_PAGE_DEFAULT = 8;
+    private readonly BOX_ITEMS_HEIGHT = 73.6;
 
     present(pagination: Pagination<Ticket>): TicketItemsType {
         return {
             selectedItems: pagination.selectedItems,
 
             itemStyle: {
-                height: `${this.BOX_ITEMS_HEIGHT / pagination.numberPerPage
+                height: pagination.numberPerPage > 8 ? '9.2rem' : `${this.BOX_ITEMS_HEIGHT / pagination.numberPerPage
                     }rem`
-            },
-            profilStyle: {
-                height: `${this.PROFIL_PHOTO_DEFAULT_DIMENSION * (this.NUMBER_PER_PAGE_DEFAULT / pagination.numberPerPage)
-                    }rem`,
-                width: `${this.PROFIL_PHOTO_DEFAULT_DIMENSION * (this.NUMBER_PER_PAGE_DEFAULT / pagination.numberPerPage)
-                    }rem`,
-
             }
         };
     }
--- src/UI/TicketTableItem/TicketTableItem.tsx
+++ src/UI/TicketTableItem/TicketTableItem.tsx
@@ -10,16 +10,34 @@ type Props = {
     ticket: Ticket;
 };
 export const TicketTableItem: FC<Props> = ({ style, ticket }) => {
+    console.log(style)
     return (
         <tr className={Css.ticketTableItem} style={style.itemStyle}>
             <td className={Css.firstCol} >
-                <div className={Css.ticketTableItemProfil} style={style.profilStyle}>
+                <div className={Css.ticketTableItemProfil}>
                     <img src={p1} alt="Profil" className={Css.ticketTableItemProfilImg} />
                 </div>
+                <div className={Css.ticketTableItemDetails}>
+                    <p className={Css.ticketTableItemDetailsDescription}>Contact Email not Linked</p>
+                    <p className={Css.ticketTableItemDetailsUpdate}>Updated 1 day ago</p>
+                </div>
             </td>
-            <td className={Css.secondCol}></td>
-            <td className={Css.thirdCol}></td>
-            <td className={Css.fourthCol}></td>
+            <td className={Css.secondCol}>
+                <div className={Css.ticketTableItemName}>
+                    <p className={Css.ticketTableItemNameValue}>Tom Cruise</p>
+                    <p className={Css.ticketTableItemNameDate}>on 24.05.2019</p>
+                </div>
+            </td>
+            <td className={Css.thirdCol}>
+
+                <div className={Css.ticketTableItemDate}>
+                    <p className={Css.ticketTableItemDateDay}>May 26, 2019</p>
+                    <p className={Css.ticketTableItemDateHour}>6:30 PM</p>
+                </div>
+            </td>
+            <td className={Css.fourthCol}>
+                <div className={Css.ticketTableItemPriorityLabel}>HIGH</div>
+            </td>
         </tr>
     );
 };
